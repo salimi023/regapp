8075a234ff0117fe96d88053aa6c732296eb534f
salimi023 <hun.saller@gmail.com>
2024-02-11 11:57:52 +0100

    Adding php version control to file

--- classes/Captcha.php
+++ classes/Captcha.php
@@ -16,15 +16,47 @@ final class Captcha
         $captcha_local = mb_strtolower($captcha, 'UTF-8');
         $today_eng = strtolower(date('l'));
 
-        $dayname = match($today_eng) {
-            'sunday' => 'vasárnap',
-            'monday' => 'hétfő',
-            'tuesday' => 'kedd',
-            'wednesday' => 'szerda',
-            'thursday' => 'csütörtök',
-            'friday' => 'péntek',
-            'saturday' => 'szombat'
-        };        
+        if(phpversion() >= '8.0') {
+            $dayname = match($today_eng) {
+                'sunday' => 'vasárnap',
+                'monday' => 'hétfő',
+                'tuesday' => 'kedd',
+                'wednesday' => 'szerda',
+                'thursday' => 'csütörtök',
+                'friday' => 'péntek',
+                'saturday' => 'szombat'
+            };
+        } else {
+            switch($today_eng) {
+                case 'sunday':
+                    $dayname = 'vasárnap';
+                    break;
+                
+                case 'monday':
+                    $dayname = 'hétfő';
+                    break;
+
+                case 'tuesday':
+                    $dayname = 'kedd';
+                    break;
+
+                case 'wednesday':
+                    $dayname = 'szerda';
+                    break;
+
+                case 'thursday':
+                    $dayname = 'csütörtök';
+                    break;
+
+                case 'friday':
+                    $dayname = 'péntek';
+                    break;
+
+                case 'saturday':
+                    $dayname = 'szombat';
+                    break;                
+            }
+        }       
         
         $return_value = $captcha_local === $dayname ? true : false;        
         
